bf04ae8100842b62c8cfb22d0e11757830f804b1
Nevada <olis0001@yandex.ru>
2023-05-05 00:04:38 +0800

    feat: написать js

---
+++ pages/index.js
@@ -0,0 +1,34 @@
+let popupButtonOpenend = document.querySelector('.profile__button-edit');
+let popup = document.querySelector('.popup');
+let pupupButtonClosed = document.querySelector('.popup__closed');
+let formElement = document.querySelector('.popup__form');
+let nameInput = document.querySelector('.popup__name');
+let jobInput = document.querySelector('.popup__profession');
+
+function displayPopup() {
+  popup.classList.toggle('popup__opened');
+  nameInput.value = nameInput.textContent;
+  jobInput.value = jobInput.textContent;
+}
+popupButtonOpenend.addEventListener('click', displayPopup);
+displayPopup();
+
+function closedPopup() {
+  popup.classList.remove('popup__opened');
+}
+
+pupupButtonClosed.addEventListener('click', closedPopup);
+closedPopup();
+
+
+
+function handleFormSubmit (evt) {
+    evt.preventDefault();
+    nameInput = document.getElementById('name').value;
+    jobInput = document.getElementById('profession').value;
+    nameInput.textContent = nameInput.value;
+    jobInput.textContent = jobInput.value;
+}
+formElement.addEventListener('submit', handleFormSubmit);
+
+
