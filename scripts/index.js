cdafe3cfecdef15806b54cdfacd24969b99f6b2b
Nevada <olis0001@yandex.ru>
2023-05-12 20:47:10 +0800

    Hotfix

--- scripts/index.js
+++ scripts/index.js
@@ -4,29 +4,24 @@ let pupupButtonClosed = document.querySelector('.popup__closed');
 let formElement = document.querySelector('.popup__form');
 let nameInput = document.getElementById('name');
 let jobInput = document.getElementById('prof');
+let profName = document.querySelector('.profile__title');
+let proffesion = document.querySelector('.profile__info-text');
 
 function displayPopup() {
   popup.classList.toggle('popup_opened');
-  nameInput.value = nameInput.textContent;
-  jobInput.value = jobInput.textContent;
+  nameInput.value = profName.textContent;
+  jobInput.value = proffesion.textContent;
 }
-
 function closedPopup() {
   popup.classList.remove('popup_opened');
 }
-
 popupButtonOpenend.addEventListener('click', displayPopup);
 pupupButtonClosed.addEventListener('click', closedPopup);
 
-
-
 function handleFormSubmit (evt) {
     evt.preventDefault();
-    nameInput = document.getElementById('name').value;
-    jobInput = document.getElementById('prof').value;
-    nameInput.textContent = nameInput.value;
-    jobInput.textContent = jobInput.value;
+    profName.textContent = nameInput.value;
+    proffesion.textContent = jobInput.value;
+    popup.classList.toggle('popup_opened');
 }
 formElement.addEventListener('submit', handleFormSubmit);
-
-
